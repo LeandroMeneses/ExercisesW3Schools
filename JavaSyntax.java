0ef90c4c98555f76c5606a7dad0c16c3c0dde279
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 20:41:14 -0300

    Java Syntax 2 exercises

---
+++ JavaSyntax.java
@@ -0,0 +1,13 @@
+public class JavaSyntax {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+//ESTE É O COMENTÁRIO UM UMA LINHA
+
+/* ESTE É O COMENTÁRIO PARA VÁRIAS LINHA
+ * 
+ *  SENAI DF
+ * 
+ */
