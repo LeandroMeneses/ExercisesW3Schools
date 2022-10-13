d7cfc11ce597073be182c7cec471d05fb94efb9b
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 20:56:26 -0300

    Java Syntax 5 exercises

---
+++ JavaVariables.java
@@ -0,0 +1,27 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+
+        //exercises 1
+        String carName = "Volvo";
+        System.out.println(carName);
+
+        //exercises 2
+        int maxSpeed = 120;
+        System.out.println(maxSpeed);
+
+        //exercises 3
+        int x = 5;
+        int y = 10;
+        System.out.println(x + y);
+
+        //exercises 4
+        int xx = 5;
+        int yy = 10;
+        int z = xx + yy;
+        System.out.println(z);
+
+        //exercises 5
+        int x5 = 5, y5 = 6, z5 = 50;
+        System.out.println(x5 + y5 + z5);
+    }
+}
