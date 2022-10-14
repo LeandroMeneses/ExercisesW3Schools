2dabe7a44ab21814242b218aaa12c28674882ca0
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 22:20:48 -0300

    JavaBooleans 2 exercises

---
+++ JavaBooleans.java
@@ -0,0 +1,15 @@
+public class JavaBooleans {
+    public static void main(String[] args) {
+        //exercicio 1
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+
+        //exercicio 2
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y);
+
+    }
+}
