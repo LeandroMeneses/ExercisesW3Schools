cede6287fea2cc943efecfcdd3b2cb76c1dbaade
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 22:07:08 -0300

    JavaStrings 6 exercises

---
+++ JavaStrings.java
@@ -0,0 +1,28 @@
+public class JavaStrings {
+    public static void main(String[] args) {
+        //exercicio 1
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+        //exercicio 2
+        String txt = "Hello";
+        System.out.println(txt.length());
+        //exercicio 3
+        System.out.println(txt.toUpperCase());
+
+        //exercico 4
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName + lastName);
+
+        //exercicio 5
+        String firstName1 = "John";
+        String lastName1 = "Doe";
+        System.out.println(firstName1.concat(lastName1));
+
+        //exervicio 6
+        String txt1 = "Hello Everybody";
+        System.out.println(txt1.indexOf("e"));
+
+    }
+}
