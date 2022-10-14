4fba5c0a775c19f1e8c7b0fbe617d5a788b273ab
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 21:21:01 -0300

    JavaDataTypes 3 exercises

---
+++ JavaDataTypes.java
@@ -0,0 +1,25 @@
+public class JavaDataTypes {
+    public static void main(String[] args) {
+
+        // EXERCICIO 1
+        int myNum = 9;
+        float myFloatNum = 8.99f;
+        char myLetter = 'A';
+        boolean myBool = false;
+        String myText = "Hello World";
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+
+        // EXERCICIO 2
+        System.out.println("byte, short, int, long, float, double, booleane charsão chamados:");
+        System.out.println("PRIMITIVE DATA TYPES.");
+
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
+    }
+}
