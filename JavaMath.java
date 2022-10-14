9e4f6a8ce138244eb2e6fef030c94a855f36ed60
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 22:15:12 -0300

    JavaMath 3 exercises

---
+++ JavaMath.java
@@ -0,0 +1,18 @@
+public class JavaMath {
+    public static void main(String[] args) {
+        //exercicio 1
+        int x = 5;
+        int y = 10;
+        Math.max(x, y);
+        System.out.println(Math.max(x, y));
+
+        //exercicio 2
+        int x1 = 16;
+        Math.sqrt(x1);
+        System.out.println(Math.sqrt(x1));
+
+        //exercicio 3
+        Math.random();
+        System.out.println(Math.random());
+    }
+}
